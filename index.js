e759dedb6bfb20f9d45395b8040a3cc4f8037769
Ahmad Fajrul Falah <fajrul452@gmail.com>
2018-10-24 09:46:27 +0700

    todolist localstorage

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ new Vue({
       var text_todo = this.newTodo.trim()
       if (text_todo) {
         this.todos.push({ text: text_todo, done: false })
+        localStorage.setItem('todos', JSON.stringify(this.todos))
       }
       this.newTodo = ''
     },
     removeTodo: function(i){
       this.todos.splice(i, 1)
+      localStorage.setItem('todos', JSON.stringify(this.todos))
     },
     doneTodo: function(i) {
       this.todos[i].done = !this.todos[i].done
+      localStorage.setItem('todos', JSON.stringify(this.todos))
     }
+  },
+  mounted: function() {
+    this.todos = JSON.parse(localStorage.getItem('todos'))
   }
 })
